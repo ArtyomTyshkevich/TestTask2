a95ccfaa12efbd0910b26bcd743faf4358c0e952
Tyshkevich Artyom <frod_1999@mail.ru>
2024-05-13 16:54:57 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using TestTask.Models;
+using TestTask.Data;
+using TestTask.Services.Interfaces;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        public readonly ApplicationDbContext db;
+        public OrderService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public async Task<Order> GetOrder()
+        {
+            return await db.Orders
+                            .Where(o => o.Quantity > 1)
+                            .OrderByDescending(o => o.CreatedAt)
+                            .FirstAsync();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await db.Orders
+                            .Include(o => o.User)
+                            .Where(o => o.User.Status == Enums.UserStatus.Active)
+                            .OrderByDescending(o => o.CreatedAt) 
+                            .ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,36 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        public readonly ApplicationDbContext db;
+        public UserService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public async Task<User> GetUser()
+        {
+
+            return await db.Users
+                            .Include(u => u.Orders)
+                            .OrderByDescending(u => u.Orders
+                                .Where(o => o.CreatedAt.Year == 2003 && o.Status == Enums.OrderStatus.Delivered)
+                                .Sum(o => o.Price*o.Quantity))
+                            .FirstAsync();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await db.Users
+                            .Include(u => u.Orders)
+                            .Where(u => u.Orders
+                                .Any(o => o.CreatedAt.Year == 2010 && o.Status == Enums.OrderStatus.Paid))
+                            .ToListAsync();
+        }
+    }
+}
